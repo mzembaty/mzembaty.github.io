c1c988e945de7ded83166739b76227e2ec476636
Martin Zembaty <m.zembaty@posteo.de>
2020-01-10 00:37:27 +0100

    Updates

---
+++ precache-manifest.9aeeef5c8385d558479671ed6ef54038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebff55abadaa4b02985bed45dd17b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ea20ecd84039a9bf02e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "546e1b5dc745e8000938",
+    "url": "/static/js/2.ec517b86.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ec517b86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ea20ecd84039a9bf02e",
+    "url": "/static/js/main.30d73e02.chunk.js"
+  },
+  {
+    "revision": "183bdabdec7dc54d749f",
+    "url": "/static/js/runtime-main.544078fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
